3edfd9ca05cdbb7c66da3986227e27036f1b7edb
wenderson silva <wendersontc@gmail.com>
2019-06-19 12:39:56 -0300

    novo componente kafka streaming server

---
+++ kafka-microservice/src/main/java/com/microservices/kafkamicroservice/KafkaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.kafkamicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMicroserviceApplication.class, args);
+	}
+
+}
---
+++ kafka-microservice/src/test/java/com/microservices/kafkamicroservice/KafkaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.kafkamicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

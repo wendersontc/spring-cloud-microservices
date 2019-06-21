328a48a82f8dd75e84bdb6729c91d4e774a4f803
wenderson silva <wendersontc@gmail.com>
2019-06-20 22:37:05 -0300

    add logback - kafka - elasticsearch pedidos

--- gateway-microservice/src/main/java/com/microservices/gatewaymicroservice/GatewayMicroserviceApplication.java
+++ gateway-microservice/src/main/java/com/microservices/gatewaymicroservice/GatewayMicroserviceApplication.java
@@ -2,12 +2,22 @@ package com.microservices.gatewaymicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class GatewayMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayMicroserviceApplication.class, args);
 	}
+	
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
+	} 
 
 }
